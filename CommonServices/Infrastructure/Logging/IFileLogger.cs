feaae384217276f320eeab4152b003a3ffc793e9
peterGospodinov <peter.gospodinov@gmail.com>
2024-11-01 09:03:34 +0200

    Add project files.

---
+++ CommonServices/Infrastructure/Logging/IFileLogger.cs
@@ -0,0 +1,8 @@
+﻿namespace CommonServices.Infrastructure.Logging
+{
+    public interface IFileLogger
+    {
+        void Log(string message);
+    }
+
+}
---
+++ IFileLogger.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public interface IFileLogger
+{
+    void Log(string message);
+}
